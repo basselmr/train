6bf71155db48cdb47c05ca1ccdc5fbd2f7202ce3
M Bassel Madarati <86240488+basselmr@users.noreply.github.com>
2022-11-21 11:16:30 +0300

    Update Course1.py
    json

--- Course1.py
+++ Course1.py
@@ -150,12 +150,12 @@
 #--------------------------------------
 #            JSON Object in PUTHON     
 #--------------------------------------
-#import json
-##some JSON:
-#x =  '{ "name":"John", "age":30, "city":"New York"}'
-## parse x:
-#y = json.loads(x)
-## the result is a Python dictionary:
-#print(y["age"])
+import json
+#some JSON:
+x =  '{ "name":"John", "age":30, "city":"New York"}'
+# parse x:
+y = json.loads(x)
+# the result is a Python dictionary:
+print(y["age"])
 
 
